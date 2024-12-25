cc77880157a8f657e19bca073d1ab4bbc8cd19e2
dipakPjs <deepakpokharel811@gmail.com>
2024-12-26 03:35:21 +0545

    some updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HomePageComponent from "@/components/HomePage.component";
+import WorkComponent from "@/components/Work.component";
 
 
 export default function Home() {
   return (
    <>
    <HomePageComponent />
+   <WorkComponent />
    </>
   );
 }
---
+++ src/components/Work.component.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { EmblaOptionsType } from "embla-carousel";
+
+import EmblaCarousel from "./EmblaCarousel.component";
+ 
+
+const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true };
+
+const WorkComponent: React.FC = () => {
+  return (
+    <div
+      className="relative w-full min-h-[100vh] flex justify-center items-center py-100px bg-[green]"
+      id="work"
+    >
+      {/* Carousel starts */}
+      <div className="w-full  h-full">
+        <EmblaCarousel options={OPTIONS}/>
+      </div>
+      {/* Carousel ends */}
+    </div>
+  );
+};
+
+export default WorkComponent;
