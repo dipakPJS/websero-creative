3ec7a00561312111402486bcac924e89412437b2
dipakPjs <deepakpokharel811@gmail.com>
2024-12-28 00:19:12 +0545

    some updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,14 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // Disable or relax specific ESLint rules for production builds
-      "no-console": "off", // Allows console statements
-      "no-debugger": "off", // Allows debugger statements
-      "@next/next/no-img-element": "off", // Disables Next.js img element rule
-    },
-  },
 ];
 
 export default eslintConfig;
