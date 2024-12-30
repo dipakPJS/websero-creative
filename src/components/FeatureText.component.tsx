07a44c2365bffc83cfe7174525c7ae6c6b6fccb3
dipakPjs <deepakpokharel811@gmail.com>
2024-12-30 23:27:00 +0545

    some updates

--- src/components/FeatureText.component.tsx
+++ src/components/FeatureText.component.tsx
@@ -21,7 +21,7 @@ export default function FeatureTextComponent() {
         className="absolute top-[20%] left-[0%] sm:top-[30%] sm:left-[10%]
          md:top-[10%] md:left-[10%] lg:left-[30%] lg:top-[15%] lg-1:left-[25%] border
           border-white text-sm sm:text-lg md:font-bold md:text-xl lg:text-3xl 
-          font-extralight text-slate-300 rounded-[100px] w-[150px] sm:w-[150px] md:w-[190px] lg:w-[200px] text-center backdrop-blur-20 p-1 py-2 md:p-2 lg:p-2 ">
+          font-extralight text-slate-300 rounded-[100px] min-w-[100px] sm:w-[150px] md:w-[190px] lg:w-[200px] text-center backdrop-blur-20 p-1 py-2 md:p-2 lg:p-2 ">
           UI/UX Design
         </motion.p>
         <motion.p
@@ -32,17 +32,17 @@ export default function FeatureTextComponent() {
         className="absolute top-[20%] right-[0%] sm:top-[30%] sm:left-[65%]
          md:top-[10%] md:left-[65%] lg:left-[40%] lg:top-[28%] lg-1:left-[35%] border
           border-white text-sm sm:text-lg md:font-bold md:text-xl lg:text-3xl 
-          font-extralight text-slate-300 rounded-[100px] w-[100px] sm:w-[150px] md:w-[190px] lg:w-[200px] text-center backdrop-blur-20 p-1  py-2 md:p-2 lg:p-2">
+          font-extralight text-slate-300 rounded-[100px] min-w-[100px] sm:w-[150px] md:w-[190px] lg:w-[200px] text-center backdrop-blur-20 p-1  py-2 md:p-2 lg:p-2">
          Branding
         </motion.p>
         <motion.p
           initial={fadeInDirections.right.initial}
-          whileInView={fadeInDirections.right.animate}
+          whileInView={fadeInDirections.right.animate} 
           viewport={{ once: false, amount: 0.2 }}
           transition={{ duration: 0.6, delay: 0.6 }}
         className="absolute top-[40%] left-[30%] sm:top-[40%] sm:left-[37.5%] md:top-[20%] md:left-[39%] lg:left-[60%] lg:top-[41%] lg-1:left-[50%] border
           border-white text-sm sm:text-lg md:font-bold md:text-xl lg:text-3xl 
-          font-extralight text-slate-300 rounded-[100px] w-[100px] sm:w-[150px] md:w-[190px] lg:w-[200px] text-center backdrop-blur-20 p-1  py-2 md:p-2 lg:p-2">
+          font-extralight text-slate-300 rounded-[100px] min-w-[100px] sm:w-[150px] md:w-[190px] lg:w-[200px] text-center backdrop-blur-20 p-1  py-2 md:p-2 lg:p-2">
        Web
         </motion.p>
         <motion.p
@@ -52,7 +52,7 @@ export default function FeatureTextComponent() {
           transition={{ duration: 0.6, delay: 0.8 }}
         className="absolute top-[60%] left-[0%] sm:top-[60%] sm:left-[10%] md:top-[60%] md:left-[10%] lg:left-[45%] lg:top-[54%] lg-1:left-[35%] border
           border-white text-sm sm:text-lg md:font-bold md:text-xl lg:text-3xl 
-          font-extralight text-slate-300 rounded-[100px] w-[100px] sm:w-[150px] md:w-[190px] lg:w-[200px] text-center backdrop-blur-20 p-1  py-2 md:p-2 lg:p-2">
+          font-extralight text-slate-300 rounded-[100px] min-w-[100px] sm:w-[150px] md:w-[190px] lg:w-[200px] text-center backdrop-blur-20 p-1  py-2 md:p-2 lg:p-2">
      SMM
         </motion.p>
         <motion.p
@@ -62,7 +62,7 @@ export default function FeatureTextComponent() {
           transition={{ duration: 0.6, delay: 1 }}
         className="absolute top-[60%] right-[0%] sm:top-[60%] sm:left-[62%] md:top-[60%] md:left-[65%] lg:left-[20%] lg:top-[67%] lg-1:left-[10%] border
           border-white text-sm sm:text-lg md:font-bold md:text-xl lg:text-3xl 
-          font-extralight text-slate-300 rounded-[100px] w-[150px] sm:w-[150px] md:w-[190px] lg:w-[200px] text-center backdrop-blur-20 p-1  py-2 md:p-2 lg:p-2">
+          font-extralight text-slate-300 rounded-[100px] min-w-[100px] sm:w-[150px] md:w-[190px] lg:w-[200px] text-center backdrop-blur-20 p-1  py-2 md:p-2 lg:p-2">
      Chatbot
         </motion.p>
 
