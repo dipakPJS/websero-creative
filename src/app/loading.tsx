b75dbe74441a222abd4fd9c35fa82297c0826537
dipakPjs <deepakpokharel811@gmail.com>
2025-01-10 06:34:36 +0545

    some updates

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@
 
  export default function Loading() {
     return (
-      <div className="flex items-center justify-center h-screen w-full bg-black relative">
+      <div className="flex items-center justify-center min-h-screen w-full bg-black relative">
        <div className="absolute h-full w-full flex justify-center items-center z-[9999]">
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-slate-300"></div>
     
