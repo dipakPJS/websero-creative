a2ec18ede2bb587ad733fed50ea11fdba43f3518
dipakPjs <deepakpokharel811@gmail.com>
2025-01-31 04:13:01 +0545

    some updates

--- src/components/Intro.component.tsx
+++ src/components/Intro.component.tsx
@@ -18,15 +18,15 @@ const fastLoadingVariants = {
 
 export default function IntroComponent() {
   return (
-    <motion.div className="flex flex-col gap-[50px] bg-[red] text-center"
+    <motion.div className="flex flex-col gap-[50px] text-center"
     variants={fastLoadingVariants}
     initial="hidden"
     whileInView="show"
     viewport={{ once: false, amount: 0.2 }}
     >
-      <div className="flex flex-col md:text-center w-full lg:w-[65%] bg-[blue] mx-auto lg:mx-0">
+      <div className="flex flex-col md:text-center w-[80%] lg:w-[65%] mx-auto lg:mx-0">
         <FlipWordsComponent />
-        <p className=" text-center lg-1:text-left lg:text-left mt-[30px] lg-1:mt-[20px] lg:mt-[20px] text-md md:text-4xl lg:text-3xl font-bold  text-slate-400 w-full">
+        <p className=" text-center lg-1:text-left lg:text-left mt-[15px] lg-1:mt-[20px] lg:mt-[20px] text-md md:text-4xl lg:text-3xl font-bold  text-slate-400 w-full">
           Enhancing Your Online Presence – Distinguish Yourself on the Web and
           Attract Potential Clients
         </p>
