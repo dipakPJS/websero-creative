bd622e6e59eae720fdb3690988798f79c3036a95
dipakPjs <deepakpokharel811@gmail.com>
2025-01-31 13:32:38 +0545

    some updates

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@
     return (
       <div className="flex items-center justify-center min-h-screen w-full bg-black relative">
        <div className="absolute h-full w-full flex justify-center items-center z-[9999]">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-slate-300"></div>
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-dashed border-slate-300"></div>
     
        </div>
       </div>
